319216e754cb736b55d5b16d41ad39beeaacbcf6
Kanishka <kanishkasolanki456s@gmail.com>
2025-11-24 23:54:24 +0530

    made variables for registers

--- main.cpp
+++ main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <bits/chrono.h>
+int v0=0;
+int v1=0;
+int v2=0;
+int v3=0;
+int v4=0;
+int v5=0;
+int v6=0;
+int v7=0;
+int v8=0;
+int v9=0;
+int va=0;
+int vb=0;
+int vc=0;
+int vd=0;
+int ve=0;
+int vf=0;
+int memory[0xFFF];
+int* program_counter= &memory[0x200];
+
 int main() {
-    std::cout<< "This is a Chip-8 emulator";
+    
     return 0;
 }
