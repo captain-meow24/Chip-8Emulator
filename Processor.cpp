157d5632b68f4894f810ecfa22458a9ffb996aa7
Kanishka <kanishkasolanki456s@gmail.com>
2025-11-30 03:19:39 +0530

    added functions

--- Processor.cpp
+++ Processor.cpp
@@ -1,5 +1,9 @@
 //
 // Created by kanishka on 26/11/25.
 //
+#include<cstdint>
 
 #include "Processor.h"
+
+
+
--- Processor.h
+++ Processor.h
@@ -8,15 +8,28 @@
 
 
 class Processor {
+public:
     std::uint8_t registers[16]{};
     std::uint8_t memory[4096]{};
-    std::uint16_t pc;
+    std::uint16_t pc{};
     std::uint16_t stack[16]{};
     std::uint8_t sp{};
     std::uint8_t delayTimer{};
     std::uint8_t soundTimer{};
     std::uint8_t keypad[16]{};
+    std::uint16_t opcode{};
+    std::uint32_t video[64*32]{};
+    std::uint16_t index{}; // used to store the current address of the sprite
+    Processor() {
+        memory[513] = 0x66;
+        memory[514] = 0x12;
+    }
 
+
+
+    void fetch();
+    void Join_instruction();
+    void execute();
 };
 
 
